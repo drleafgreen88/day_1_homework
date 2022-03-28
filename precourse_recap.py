dd7675caaee8c00fefc57e0f9bcfcf40f347e179
Derek Leafgreen <drl21788@gmail.com>
2022-03-28 18:25:30 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,11 @@
+def my_sentence():
+    sentence = "This is my first day!"
+    print(sentence)
+
+my_sentence()
+
+def my_favoritenumber():
+    my_favno = 4 * 20
+    print(my_favno)
+
+my_favoritenumber()
